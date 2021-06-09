5021c13efc2f56481f9824a20d5ea4883d45bfe7
Cayce House <cayce@mobilemedics.com>
2021-06-09 05:31:14 +0000

    Fixed if statement in capacity

--- lib/minecraft-cdk-stack.capacity.ts
+++ lib/minecraft-cdk-stack.capacity.ts
@@ -11,8 +11,11 @@ exports.handler = function (event: { [x: string]: string; }, context: { logStrea
             DesiredCapacity: capacity
         };
         autoscaling.setDesiredCapacity(params, function (err, data) {
-            if (err) console.log(err, err.stack);
-            else console.log(data);
+            if (err) {
+                console.log(err, err.stack);
+            } else {
+                console.log(data);
+            }
         });
     }
 
